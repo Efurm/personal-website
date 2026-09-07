1e6166a3cd55f10fe3caa37f6ff7b4004c4daccc
Ethan Furman <efurman1@buffs.wamu.edu>
2026-09-06 23:35:16 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+ToDoList = [
+    {"name": "Fill up car", "completed": True},
+    {"name": "Complete class homeworks", "completed": False},
+    {"name": "Contact project group", "completed": False},
+    {"name": "Read the study guide", "completed": True},
+    {"name": "Attend meeting", "completed": False},
+]
+
 # Create your views here.
 def about(request):
     return render(request, "about.html")
 def contact(request):
     return render(request, "contact.html")
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"ToDoList": ToDoList})
